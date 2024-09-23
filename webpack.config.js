6c366a134a39cc0b184fdd14624e4076c63fca35
Ulyana <ulyalazutina@yandex.ru>
2024-09-23 12:27:15 +0300

    Установка webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const devServer = (isDev) => isDev && {
+    devServer: {
+        port: 3005,
+        open: true,
+        hot: true,
+        watchFiles: '[./src/*]',
+    }
+}
+
+
+module.exports = ({ develop }) => ({
+    mode: develop ? 'development' : 'production',
+    entry: path.resolve(__dirname, "src/js/index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: '[name].[contenthash:8].js',
+        chunkFilename: '[id].[hash:8].js',
+        assetModuleFilename: 'media/[name][hash][ext][query]',
+        clean: develop ? false : true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/index.html'),
+            chunks: ['index'],
+        }),
+        new MiniCssExtractPlugin({ filename: develop ? 'css/main.css' : 'css/[name].[contenthash:8].css' }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(?:png, jpg, jpeg, svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[hash][ext][query]'
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    ...devServer(develop),
+});
